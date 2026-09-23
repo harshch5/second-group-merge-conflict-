7d28f6654f9fdb5dcbdaa1142255879963bfc49d
harshitamaheshwari-cloud <harshita.maheswari@appinventiv.com>
2026-09-23 23:24:28 +0530

    :q Added multiplication code to new branch
    :q
    
    :q
    :wq

--- file.js
+++ file.js
@@ -6,3 +6,12 @@ let num1 = 30;
 
 let result = calculate(num1);
 console.log(result);
+function mul(a,b){
+    return a*b;
+}
+
+
+let a=20;
+let b=5;
+console.log(mul(a,b));
+console.log(divide(a,b));
